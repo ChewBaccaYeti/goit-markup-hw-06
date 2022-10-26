2f0664744b5f4e472714dcfc6c11b124cdf8b41b
Venedykt Mitishov <chewbaccayeti@gmail.com>
2022-10-26 22:35:05 +0300

    6th HW my own has been completed

--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    backdrop: document.querySelector('[data-backdrop]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("body"),
   };
 
-  refs.openModalBtn.addEventListener('click', openModal);
-  refs.closeModalBtn.addEventListener('click', closeModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function openModal() {
-    refs.backdrop.classList.add('visible');
-  }
-  function closeModal() {
-    setTimeout(() => refs.backdrop.classList.remove('visible'), 500);
-    setTimeout(() => refs.backdrop.classList.remove('backdrop-close-anim'), 500);
-    setTimeout(() => refs.modal.classList.remove('modal-close-anim'), 500);
-    refs.backdrop.classList.add('backdrop-close-anim');
-    refs.modal.classList.add('modal-close-anim');
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll")
   }
 })();
